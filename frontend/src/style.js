7029f7c83ddd604231906f43181e1765f24e80d8
grjus <grjus_services+github@pm.me>
2021-04-12 13:55:38 +0200

    Fixed global style for container

--- frontend/src/style.js
+++ frontend/src/style.js
@@ -4,15 +4,24 @@ export const theme = {
   mainLayoutColor: '#E3E2DF',
   color: 'rgba(0, 0, 0, 0.8)',
   // topBarColor: '#BAB2B5',
-  topBarColor: 'white',
+  topBarColor: 'rgba(255,250,250,0.8)',
   logoColor: '#123C69',
   logoColorHover: '#AC3B61',
-
+  primary: '#123C69',
+  secondary: '#AC3B61',
+  deviceMin: '450px',
 };
 
 const GlobalStyle = createGlobalStyle`
 *{
   box-sizing:border-box;
+}
+:root{
+  font-size:16px;
+  @media (max-width:${theme.deviceMin}){
+      font-size:14px;
+    }
+
 }
   body {
     font-family:'Roboto','Arial',sans-serif;
@@ -25,20 +34,23 @@ const GlobalStyle = createGlobalStyle`
     letter-spacing:0.5px;
     color:rgba(0,0,0,0.6);
 
+
   }
 `;
 
 export const Container = styled.div`
-display:block;
-background-color:${(props) => (props.noBackColor ? theme.mainLayoutColor : 'whitesmoke')};
-position:relative;
-top:40px;
-bottom:0;
-left:0;
-right:0;
-margin:auto;
-transition:height 0.5s ease-in;
-${({ hidden }) => hidden && `
+  display: block;
+  background-color: ${(props) => (props.noBackColor ? theme.mainLayoutColor : 'whitesmoke')};
+  position: relative;
+  top: 40px;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  width: 70%;
+  transition: height 0.5s ease-in;
+  ${({ hidden }) => hidden
+    && `
 height:300px;
 `}
 `;
